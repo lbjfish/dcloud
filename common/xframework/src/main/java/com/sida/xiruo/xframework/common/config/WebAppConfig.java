8d9bbf4f70c8cd0f1070128868f3a2a4200c92fb
jianglingfeng <jianglingfeng@szida.com>
2018-11-13 09:21:51 +0800

    1. 跨域处理

--- common/xframework/src/main/java/com/sida/xiruo/xframework/common/config/WebAppConfig.java
+++ common/xframework/src/main/java/com/sida/xiruo/xframework/common/config/WebAppConfig.java
@@ -3,10 +3,12 @@ package com.sida.xiruo.xframework.common.config;
 import com.sida.xiruo.xframework.common.aop.AllowCrossDomainInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
+//改用过滤器方式，见CorsConfig
+//@Configuration
 public class WebAppConfig extends WebMvcConfigurerAdapter {
 
     @Autowired
@@ -19,4 +21,9 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
         registry.addInterceptor(allowCrossDomainInterceptor).addPathPatterns(apiUri);
     }
 
+    // 跨域
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH").allowCredentials(true).maxAge(3600);
+    }
 }
